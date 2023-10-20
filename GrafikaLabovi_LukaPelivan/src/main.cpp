1277b7d0d0b3e4b88a216357fb2aca7210bb8492
lpeliv <Luka@pelivan.com>
2023-10-20 12:01:02 +0200

    Third commit

--- GrafikaLabovi_LukaPelivan/src/main.cpp
+++ GrafikaLabovi_LukaPelivan/src/main.cpp
@@ -14,92 +14,33 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
 int main()
 {
 
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
-
-    //_________________________________________________________________
-    //My addition for 2nd exercise
-    float finput = 0;
-    int polygon = 0.0f;
-    std::vector<float> vertices;
-    std::vector<float> triangles;
-    std::string line;
-
-    std::cout << "What's your object name?: ";
-    std::string objectName;
-    std::cin >> objectName;
-
-    std::ofstream file(objectName + ".obj");
-    file << "# " + objectName + ".obj created by input." << std::endl << std::endl;;
-    file << "g " + objectName << std::endl << std::endl;
-
-    for (int i = 0; i <= 2; i++) {
-        file << "v ";
-        for (int j = 0; j < 2; j++) {
-
-            std::cout << "What's your [" << i + 1 << "," << j + 1 << "] vertex location (0.x format)? : ";
-            std::cin >> finput;
-            file << finput << "0 ";
-        }
-
-        file << "0.0" << std::endl;
-    }
-
-    file << std::endl;
-
-    for (int i = 0; i < 2; i++) {
-        file << "f";
-        for (int j = 0; j < 3; j++) {
-            file << " ";
-            std::cout << "What's your ["<< i + 1 << "," << j + 1 << "] polygon (x format)? : ";
-            std::cin >> polygon;
-            file << polygon;
-        }
-        
-        file << std::endl;
-    }
-
-    file.close();
-
-    std::ifstream file2(objectName + ".obj");
-    while (getline(file2, line)) {
-        if (line.find('v') != std::string::npos) {
-            line.erase(0, line.find('v '));
-
-            std::istringstream iss(line);
-            std::copy(std::istream_iterator<float>(iss),
-                std::istream_iterator<float>(),
-                std::back_inserter(vertices));
-
-            std::copy(vertices.begin(), vertices.end(),
-                std::ostream_iterator<float>(std::cout, "f, "));
-            std::cout << "\n";
-        }
-
-        else if (line.find('f') != std::string::npos) {
-            line.erase(0, line.find('f'));
-            std::istringstream iss(line);
-            std::copy(std::istream_iterator<float>(iss),
-                std::istream_iterator<float>(),
-                std::back_inserter(triangles));
-
-            std::copy(triangles.begin(), triangles.end(),
-                std::ostream_iterator<float>(std::cout, ", "));
-            std::cout << "\n";
-        }
-    }
-
-    file.close();
-    //________________________________________________________________
-    //End of my addition
-    
-
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     // glfw window creation
     // --------------------
@@ -120,11 +61,146 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
-    // render loop
-    // -----------
+
+    //_________________________________________________________________
+    //My addition for 2nd exercise
+    float finput = 0;
+    int polygon = 0.0f;
+    std::vector<float> vertices;
+    std::vector<unsigned int> triangles;
+    std::string line;
+
+    std::cout << "What's your object name?: ";
+    std::string objectName;
+    std::cin >> objectName;
+
+    //-------------------------------
+    // USER INPUT
+    //std::ofstream file(objectName + ".obj");
+    //std::string objectName = "triangle.obj";
+    /*std::ofstream file(objectName + ".obj");
+    file << "# " + objectName + ".obj created by input." << std::endl << std::endl;;
+    file << "g " + objectName << std::endl << std::endl;
+
+    for (int i = 0; i <= 2; i++) {
+        file << "v ";
+        for (int j = 0; j < 2; j++) {
+
+            std::cout << "What's your [" << i + 1 << "," << j + 1 << "] vertex location (0.x format)? : ";
+            std::cin >> finput;
+            file << finput << "0 ";
+        }
+
+        file << "0.0" << std::endl;
+    }
+
+    file << std::endl;
+
+    for (int i = 0; i < 1; i++) {
+        file << "f";
+        for (int j = 0; j < 3; j++) {
+            file << " ";
+            std::cout << "What's your [" << i + 1 << "," << j + 1 << "] polygon (x format)? : ";
+            std::cin >> polygon;
+            file << polygon;
+        }
+
+        file << std::endl;
+    }
+
+    file.close();*/
+    //-------------------------------
+
+
+    std::ifstream file2(objectName);
+    while (getline(file2, line)) {
+        if (line.find('v') != std::string::npos) {
+            line.erase(0, line.find('v '));
+
+            std::istringstream iss(line);
+            std::copy(std::istream_iterator<float>(iss),
+                std::istream_iterator<float>(),
+                std::back_inserter(vertices));
+        }
+
+        else if (line.find('f') != std::string::npos) {
+            line.erase(0, line.find('f '));
+            std::istringstream iss(line);
+            std::copy(std::istream_iterator<unsigned int>(iss),
+                std::istream_iterator<unsigned int>(),
+                std::back_inserter(triangles));
+        }
+    }
+
+    float* vArray = vertices.data();
+    unsigned int* vTriangles = triangles.data();
+    int n_verts = vertices.size();
+    int n_faces = triangles.size();
+
+    //________________________________________________________________
+    //End of my addition
+
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, n_verts* sizeof(float), vArray, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, n_faces* sizeof(unsigned int), vTriangles, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     while (!glfwWindowShouldClose(window))
-    {   
+    {
         // input
         // -----
         processInput(window);
@@ -133,16 +209,26 @@ int main()
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, n_faces, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time 
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
-
     }
-
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
     glfwTerminate();
 return 0;
 }
