19819dfde6b8ba6e56aed06634a1537e38d6cad7
lpeliv <92396416+lpeliv@users.noreply.github.com>
2023-12-07 17:44:23 +0100

    Finished task 3 with a plane model

--- Vjezba-4/GrafikaLabovi_LukaPelivan/src/main.cpp
+++ Vjezba-4/GrafikaLabovi_LukaPelivan/src/main.cpp
@@ -34,17 +34,40 @@ int main()
 
         //---------
 
-        glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+        //Task 3
+        //--------------------------------------------
+        glm::mat4 modelM = glm::mat4(1.0f);
+        modelM = glm::rotate(modelM, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shader.GetID(), "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelM));
+
+        int viewLoc = glGetUniformLocation(shader.GetID(), "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projectionLoc = glGetUniformLocation(shader.GetID(), "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        //--------------------------------------------
+
+
+        //Task 2
+        //--------------------------------------------
+        /*glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
         glm::mat4 trans = glm::mat4(1.0f);
         trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
         trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
         trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
         vec = trans * vec;
-
+        
         unsigned int transformLoc = glGetUniformLocation(shader.GetID(), "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
-        //---------
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));*/
+        //--------------------------------------------
 
         model.Draw(shader, tex);
 
