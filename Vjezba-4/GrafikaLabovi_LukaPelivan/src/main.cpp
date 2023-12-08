6543001c45c1266163cf00a13f85bf4d0dff5110
lpeliv <Luka@pelivan.com>
2023-12-08 10:59:52 +0100

    Updated Task 4 (AWSD movement)

--- Vjezba-4/GrafikaLabovi_LukaPelivan/src/main.cpp
+++ Vjezba-4/GrafikaLabovi_LukaPelivan/src/main.cpp
@@ -15,7 +15,7 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-void processInputJerJeRokovSmece(GLFWwindow* window, glm::vec3 cameraPos, glm::vec3 cameraFront, glm::vec3 cameraUp, float deltaTime);
+void processInputJerJeRokovSmece(GLFWwindow* window, glm::vec3* cameraPos, glm::vec3* cameraFront, glm::vec3* cameraUp, float deltaTime);
 
 
 //Camera variables
@@ -67,7 +67,7 @@ int main()
         
         
         glm::mat4 view = glm::mat4(1.0f);
-        //Comented for task 4 - 3
+        //Comented for task 4 - 4
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         
         glm::mat4 projection;
@@ -76,10 +76,10 @@ int main()
         //Comented for task 3 - 3D
         /*int modelLoc = glGetUniformLocation(shader.GetID(), "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelM));*/
-        
+        /*
 
         int viewLoc = glGetUniformLocation(shader.GetID(), "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));*/
 
         int projectionLoc = glGetUniformLocation(shader.GetID(), "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
@@ -139,33 +139,37 @@ int main()
 
         //Task 4-3 rotate camera around object
         //--------------------------------------------
-        glm::mat4 trans = glm::mat4(1.0f);
+        /*glm::mat4 trans = glm::mat4(1.0f);
         trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
         unsigned int transformLoc = glGetUniformLocation(shader.GetID(), "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
-        //A:
-        /*glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
         
         const float radius = 10.0f;
         float camX = sin(glfwGetTime()) * radius;
         float camZ = cos(glfwGetTime()) * radius;
         view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));*/
 
-        //B:
+        //--------------------------------------------
+
+        //Task 4-4 not working
+        //--------------------------------------------
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+        unsigned int transformLoc = glGetUniformLocation(shader.GetID(), "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        int viewLoc = glGetUniformLocation(shader.GetID(), "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        processInputJerJeRokovSmece(window.GetWindow(), cameraPos, cameraFront, cameraUp, deltaTime);
-        
-        //--------------------------------------------
-
-        //Task 4-4 not working
-        //--------------------------------------------
+        processInputJerJeRokovSmece(window.GetWindow(), &cameraPos, &cameraFront, &cameraUp, deltaTime);
         
         //--------------------------------------------
 
@@ -180,17 +184,17 @@ int main()
     return 0;
 }
 
-void processInputJerJeRokovSmece(GLFWwindow* window, glm::vec3 cameraPos, glm::vec3 cameraFront, glm::vec3 cameraUp, float deltaTime)
+void processInputJerJeRokovSmece(GLFWwindow* window, glm::vec3 *cameraPos, glm::vec3 *cameraFront, glm::vec3 *cameraUp, float deltaTime)
 {
     float cameraSpeed = 2.5f * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraFront;
+        *cameraPos += cameraSpeed * *cameraFront;
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraFront;
+        *cameraPos -= cameraSpeed * *cameraFront;
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        *cameraPos -= glm::normalize(glm::cross(*cameraFront, *cameraUp)) * cameraSpeed;
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        *cameraPos += glm::normalize(glm::cross(*cameraFront, *cameraUp)) * cameraSpeed;
 }
